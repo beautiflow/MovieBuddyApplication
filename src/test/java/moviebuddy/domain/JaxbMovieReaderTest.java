466142050f570a092afc7d024c1ab6e285c8c4e1
beautiflow <1114ny@naver.com>
2022-09-29 11:15:28 +0900

    Section.8

--- src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
+++ src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
@@ -1,11 +1,17 @@
 package moviebuddy.domain;
 
+import moviebuddy.MovieBuddyFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.List;
 
+@SpringJUnitConfig(MovieBuddyFactory.class)
 public class JaxbMovieReaderTest {
+
+    @Autowired JaxbMovieReader movieReader;
 
     @Test
     void NotEmpty_LoadedMovies(){
