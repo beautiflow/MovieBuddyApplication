e992712eed58fdfdf0e7461e8b963faf54990ae8
beautiflow <1114ny@naver.com>
2022-10-13 10:17:15 +0900

    Section.13-2 중간까지 내용

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -2,7 +2,9 @@ package moviebuddy;
 
 import moviebuddy.data.CsvMovieReader;
 import moviebuddy.data.XmlMovieReader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.oxm.Unmarshaller;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
@@ -12,6 +14,7 @@ import java.net.URISyntaxException;
 @Configuration // 이렇게 class 레벨에 @Configuration 을 붙이면 이 클래스는 이제 스프링의 빈 구성정보, 즉 Configuration metadata 로 사용이 될 수 있음을 선언.
 @ComponentScan(basePackages = { "moviebuddy" }) // 스프링 컨테이너에 빈을 자동으로 등록해주는 기능을 제공
 @Import({ MovieBuddyFactory.DomainModuleConfig.class, MovieBuddyFactory.DataSourceModuleConfig.class }) // @Import 는 다른 클래스에서 빈 구성 정보를 불러오기 위해서 사용하는 것
+
 public class MovieBuddyFactory {  // 객체를 생성하고 구성하는 역할
 
     @Bean
@@ -30,11 +33,19 @@ public class MovieBuddyFactory {  // 객체를 생성하고 구성하는 역할
     @Configuration
     static class DataSourceModuleConfig{
 
+        private final Environment environment;
+
+        public DataSourceModuleConfig(Environment environment){
+            this.environment = environment;
+        }
+
         @Profile(MovieBuddyProfile.CSV_MODE)
         @Bean
         public CsvMovieReader csvMovieReader() {
             CsvMovieReader movieReader = new CsvMovieReader();
-            movieReader.setMetadata("movie_metadata.csv");
+
+            // 애플리케이션 외부에서 작성된 설정정보를 읽어, 메타데이터 위치 설정하기
+            //movieReader.setMetadata(environment.getProperty("movie.metadata"));
 
             return movieReader;
         }
@@ -43,7 +54,7 @@ public class MovieBuddyFactory {  // 객체를 생성하고 구성하는 역할
         @Bean
         public XmlMovieReader xmlMovieReader(Unmarshaller unmarshaller) {
             XmlMovieReader movieReader = new XmlMovieReader(unmarshaller);
-            movieReader.setMetadata("movie_metadata.xml");
+           // movieReader.setMetadata(environment.getProperty("movie.metadata"));
 
             return movieReader;
         }
