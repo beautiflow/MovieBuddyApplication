372d2f63f5d61e1e5e3e5e342561d0f9f09455b1
beautiflow <1114ny@naver.com>
2022-10-13 11:10:04 +0900

    Section.13

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -12,6 +12,7 @@ import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
 
 @Configuration // 이렇게 class 레벨에 @Configuration 을 붙이면 이 클래스는 이제 스프링의 빈 구성정보, 즉 Configuration metadata 로 사용이 될 수 있음을 선언.
+@PropertySource("/application.properties")
 @ComponentScan(basePackages = { "moviebuddy" }) // 스프링 컨테이너에 빈을 자동으로 등록해주는 기능을 제공
 @Import({ MovieBuddyFactory.DomainModuleConfig.class, MovieBuddyFactory.DataSourceModuleConfig.class }) // @Import 는 다른 클래스에서 빈 구성 정보를 불러오기 위해서 사용하는 것
 
@@ -33,31 +34,7 @@ public class MovieBuddyFactory {  // 객체를 생성하고 구성하는 역할
     @Configuration
     static class DataSourceModuleConfig{
 
-        private final Environment environment;
-
-        public DataSourceModuleConfig(Environment environment){
-            this.environment = environment;
-        }
-
-        @Profile(MovieBuddyProfile.CSV_MODE)
-        @Bean
-        public CsvMovieReader csvMovieReader() {
-            CsvMovieReader movieReader = new CsvMovieReader();
-
-            // 애플리케이션 외부에서 작성된 설정정보를 읽어, 메타데이터 위치 설정하기
-            //movieReader.setMetadata(environment.getProperty("movie.metadata"));
-
-            return movieReader;
-        }
-
-        @Profile(MovieBuddyProfile.XML_MODE)
-        @Bean
-        public XmlMovieReader xmlMovieReader(Unmarshaller unmarshaller) {
-            XmlMovieReader movieReader = new XmlMovieReader(unmarshaller);
-           // movieReader.setMetadata(environment.getProperty("movie.metadata"));
-
-            return movieReader;
         }
 
     }
-}
+
--- src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -4,13 +4,8 @@ import moviebuddy.MovieBuddyFactory;
 import moviebuddy.MovieBuddyProfile;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.List;
